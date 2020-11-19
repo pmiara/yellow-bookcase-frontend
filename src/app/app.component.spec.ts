c0e520d68f77e54b5a99e93e9b9718bec52120f8
pmiara <piotr.miara97@gmail.com>
2020-11-19 21:32:08 +0100

    Delete the placeholder code from AppComponent's template

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,8 +26,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain(
-      'panel-frontend app is running!'
+    expect(compiled.querySelector('h1').textContent).toContain(
+      'panel-frontend'
     );
   });
 });

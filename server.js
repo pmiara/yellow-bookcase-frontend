ed786a0b2b31cb0d76c84c904ab04a3b94c1d602
pmiara <piotr.miara97@gmail.com>
2020-11-19 11:41:38 +0100

    Set ESlint and Prettier as default linter and formatter respectively

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static('./dist/panel-frontend'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', { root: 'dist/panel-frontend/' }),
+  res.sendFile('index.html', { root: 'dist/panel-frontend/' })
 );
 
 app.listen(process.env.PORT || 8080);

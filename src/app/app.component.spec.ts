a57dd86e7d3cd446a083a5c3d3dd45a60c2efa6f
pmiara <piotr.miara97@gmail.com>
2020-11-26 11:05:07 +0100

    Change app title to Żółta Biblioteczka

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,11 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let comp: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule],
@@ -11,23 +14,22 @@ describe('AppComponent', () => {
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'panel-frontend'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('panel-frontend');
+  it(`should have as title 'Żółta Biblioteczka'`, () => {
+    fixture = TestBed.createComponent(AppComponent);
+    comp = fixture.componentInstance;
+    expect(comp.title).toEqual('Żółta Biblioteczka');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  it('should render a title', () => {
+    fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain(
-      'panel-frontend'
+    expect(fixture.nativeElement.querySelector('h1').textContent).toContain(
+      'Żółta Biblioteczka'
     );
   });
 });

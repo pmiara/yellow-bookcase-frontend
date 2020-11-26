d49876df6a18a152763ef3f7817af6293dc24bb3
pmiara <piotr.miara97@gmail.com>
2020-11-26 10:55:33 +0100

    Add a pipe for appending host name to image URLs
    Host names are missing in the case of the local provider, which means storing images on the same server as the backend. In the case of non-local providers, e.g. AWS S3, host names are already included in the URLs.

---
+++ src/app/dashboard/cover-img-full-url.pipe.spec.ts
@@ -0,0 +1,31 @@
+import { CoverImgFullUrlPipe } from './cover-img-full-url.pipe';
+import { EnvironmentService } from '../common/environment.service';
+
+describe('CoverImgFullUrlPipe', () => {
+  const fakeEnvService = {
+    getEnvConfig: () => ({ apiUrl: 'http://example.com', production: false })
+  };
+  const pipe = new CoverImgFullUrlPipe(fakeEnvService as EnvironmentService);
+
+  it('given the local image provider adds a host prefix to a cover image url', () => {
+    const coverImg = {
+      name: 'foo.jpg',
+      url: '/uploads/images/foo.jpg',
+      provider: 'local'
+    };
+
+    expect(pipe.transform(coverImg)).toBe(
+      'http://example.com/uploads/images/foo.jpg'
+    );
+  });
+
+  it('given the non-local image provider leaves an original cover image url', () => {
+    const coverImg = {
+      name: 'foo.jpg',
+      url: 'https://images.com/foo.jpg',
+      provider: 'other'
+    };
+
+    expect(pipe.transform(coverImg)).toBe('https://images.com/foo.jpg');
+  });
+});
---
+++ src/app/dashboard/cover-img-full-url.pipe.ts
@@ -0,0 +1,17 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { CoverImg } from '../common/cover-img.model';
+import { EnvironmentService } from '../common/environment.service';
+
+@Pipe({ name: 'coverImgFullUrl' })
+export class CoverImgFullUrlPipe implements PipeTransform {
+  constructor(private environmentService: EnvironmentService) {}
+
+  transform(coverImg: CoverImg): string {
+    if (coverImg.provider === 'local') {
+      const apiUrl = this.environmentService.getEnvConfig().apiUrl;
+      return apiUrl + coverImg.url;
+    } else {
+      return coverImg.url;
+    }
+  }
+}

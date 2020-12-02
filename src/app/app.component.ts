0960fc658b3a2ad28e17485b6a4cbcd11dcdb2ae
pmiara <piotr.miara97@gmail.com>
2020-12-02 16:38:02 +0100

    Set the browser title in AppComponent
    Previously it was set in index.html.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Żółta Biblioteczka';
+export class AppComponent implements OnInit {
+  public constructor(private titleService: Title) {}
+
+  ngOnInit(): void {
+    this.titleService.setTitle('Żółta Biblioteczka');
+  }
 }

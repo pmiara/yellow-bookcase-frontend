01c9dae9422e8a1b75cffdf3c124ff04b43f7e3f
pmiara <piotr.miara97@gmail.com>
2020-12-02 18:02:37 +0100

    Add the logo and the menu with basic styling

--- src/app/header/header.module.ts
+++ src/app/header/header.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MenuComponent } from './menu.component';
 
 @NgModule({
-  declarations: [HeaderComponent],
-  imports: [CommonModule],
+  declarations: [HeaderComponent, MenuComponent],
+  imports: [CommonModule, FlexLayoutModule],
   exports: [HeaderComponent]
 })
 export class HeaderModule {}

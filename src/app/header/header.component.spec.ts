5d72b8c4107ac9d4ddccc5fa66f13e17b8fc7b4e
pmiara <piotr.miara97@gmail.com>
2020-12-10 11:17:37 +0100

    Make HeaderComponent template more verbose and add basic tests

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -1,13 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HeaderComponent } from './header.component';
+import { MenuComponent } from './menu/menu.component';
 
 describe('HeaderComponent', () => {
   let fixture: ComponentFixture<HeaderComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [HeaderComponent]
+      declarations: [HeaderComponent, MenuComponent]
     }).compileComponents();
   });
 
@@ -16,9 +16,17 @@ describe('HeaderComponent', () => {
     fixture.detectChanges();
   });
 
-  it('has a title', () => {
-    expect(fixture.nativeElement.querySelector('h1').innerText).toEqual(
+  it('shows a title', () => {
+    expect(fixture.nativeElement.querySelector('.title').innerText).toEqual(
       'Żółta Biblioteczka'
     );
+  });
+
+  it('shows a logo', () => {
+    expect(fixture.nativeElement.querySelector('.logo')).toBeTruthy();
+  });
+
+  it('shows a menu', () => {
+    expect(fixture.nativeElement.querySelector('app-menu')).toBeTruthy();
   });
 });

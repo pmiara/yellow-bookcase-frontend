ebe671d06c577b8afbca168ab326852e3b55b1a4
pmiara <piotr.miara97@gmail.com>
2021-02-05 15:27:50 +0100

    Close sidenav on routing, i.e. after a user clicks a link that doesn't point to the same page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { Observable } from 'rxjs';
 import { NavigationService } from './services/navigation.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -13,8 +14,13 @@ export class AppComponent implements OnInit {
 
   constructor(
     private titleService: Title,
-    private navService: NavigationService
-  ) {}
+    private navService: NavigationService,
+    private router: Router
+  ) {
+    this.router.events.subscribe(() => {
+      this.navService.closeSidenav();
+    });
+  }
 
   ngOnInit(): void {
     this.showSidenav = this.navService.getShowSidenav();
